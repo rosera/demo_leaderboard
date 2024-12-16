79fb8e7348e8669b9c35c576bd340d1b2c58c204
rosera <askrichardrose@gmail.com>
2024-12-16 13:00:10 +0000

    Add: Web Scroll Fix

--- lib/leaderboard_page.dart
+++ lib/leaderboard_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'dart:core';
 import 'model/item_model.dart';
@@ -42,93 +44,105 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
         // title: Text(widget.title),
         title: Text(code.toUpperCase()),
       ),
-      body: RefreshIndicator(
-        onRefresh: _refreshItems,
-        child: FutureBuilder<List<Item>>(
-          future: _itemsFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (snapshot.hasData) {
-              final items = snapshot.data!;
-              return Column(
-                children: [
-                  const Text("Pull down to refresh",
-                    style: TextStyle(
-                      fontSize: 18.0,
-                     color: Colors.blueGrey,
-                    ),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        final item = items[index];
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: 30,
-                                    child: Text(
-                                      "${index+1}.",
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                        fontSize: 22.0,
-                                      ),
-                                    ),
-                                  ),
-
-                                  const SizedBox(width:10.0),
-
-                                  SizedBox(
-                                    width: 250,
-                                    child: Text(
-                                      item.name,
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                        fontSize: 22.0,
-                                      ),
-                                    ),
-                                  ),
-
-                                  const Spacer(),
-                                  SizedBox(
-                                    // width: 0,
-                                    child: Text(
-                                      "${item.score}",
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                        fontSize: 22.0,
-                                      ),
-                                    ),
-                                  ),
-
-                                ],
-
-                                // Text('Game: ${item.game}, Score: ${item.score}'),
-                              ),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              );
-            } else {
-              return const Center(child: Text('No data available'));
-            }
+      // TODO: Add ScrollConfiguration for Web Scroll
+      body: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(
+          physics: const BouncingScrollPhysics(),
+          dragDevices: {
+            PointerDeviceKind.stylus,
+            PointerDeviceKind.touch,
+            PointerDeviceKind.mouse,
+            PointerDeviceKind.trackpad
           },
+        ),
+        child: RefreshIndicator(
+          onRefresh: _refreshItems,
+          child: FutureBuilder<List<Item>>(
+            future: _itemsFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (snapshot.hasData) {
+                final items = snapshot.data!;
+                return Column(
+                  children: [
+                    const Text("Pull down to refresh",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                       color: Colors.blueGrey,
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          final item = items[index];
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      width: 30,
+                                      child: Text(
+                                        "${index+1}.",
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                          fontSize: 22.0,
+                                        ),
+                                      ),
+                                    ),
+
+                                    const SizedBox(width:10.0),
+
+                                    SizedBox(
+                                      width: 250,
+                                      child: Text(
+                                        item.name,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                          fontSize: 22.0,
+                                        ),
+                                      ),
+                                    ),
+
+                                    const Spacer(),
+                                    SizedBox(
+                                      // width: 0,
+                                      child: Text(
+                                        "${item.score}",
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                          fontSize: 22.0,
+                                        ),
+                                      ),
+                                    ),
+
+                                  ],
+
+                                  // Text('Game: ${item.game}, Score: ${item.score}'),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return const Center(child: Text('No data available'));
+              }
+            },
+          ),
         ),
       ),
     );
--- lib/utility/fetch_data.dart
+++ lib/utility/fetch_data.dart
@@ -5,16 +5,18 @@ import '../model/item_model.dart';
 
 Future<List<Item>> fetchItems() async {
   final response =
-  await http.get(Uri.parse('http://localhost:8080/leaderboard?game=quizzrr'));
+  await http.get(Uri.parse('http://localhost:8080/games/test'));
 
   print("Status: ${response.statusCode}");
 
   if (response.statusCode == 200) {
     final List<dynamic> jsonList = jsonDecode(response.body);
     return jsonList.map((item) => Item.fromJson(item)).toList();
-  } else if (response.statusCode == 404){
-    return [];
+  // }
+  // else if (response.statusCode == 404){
+    // return [];
   } else {
-    throw Exception('Failed to fetch items');
+    // throw Exception('Failed to fetch items');
+    return [];
   }
 }
